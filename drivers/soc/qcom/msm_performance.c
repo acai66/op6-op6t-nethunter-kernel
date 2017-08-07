fb5b54719fb92a5632214447dc099702491678b0
flar2 <asegaert@gmail.com>
2017-08-06 22:26:34 -0400

    msm_performance: don't limit min and max cpu freq
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -76,6 +76,10 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 	int ret;
+	const char *reset = "0:0 1:0 2:0 3:0 4:0 5:0 6:0 7:0";
+
+	if (touchboost == 0)
+		cp = reset;
 
 	if (touchboost == 0)
 		return 0;
@@ -87,7 +91,10 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	if (!(ntokens % 2))
 		return -EINVAL;
 
-	cp = buf;
+	if (touchboost == 0)
+		cp = reset;
+	else
+		cp = buf;
 
 	cpumask_clear(limit_mask);
 	for (i = 0; i < ntokens; i += 2) {
@@ -163,6 +170,10 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
 	int ret;
+	const char *reset = "0:4294967295 1:4294967295 2:4294967295 3:4294967295 4:4294967295 5:4294967295 6:4294967295 7:4294967295";
+
+	if (touchboost == 0)
+		cp = reset;
 
 	if (touchboost == 0)
 		return 0;
@@ -174,7 +185,10 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	if (!(ntokens % 2))
 		return -EINVAL;
 
-	cp = buf;
+	if (touchboost == 0)
+		cp = reset;
+	else
+		cp = buf;
 
 	cpumask_clear(limit_mask);
 	for (i = 0; i < ntokens; i += 2) {
