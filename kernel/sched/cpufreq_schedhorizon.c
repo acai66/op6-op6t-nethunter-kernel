ed8cf67c5a2bfad8ceb1fcc73758096809ab1d12
boos4721 <3.1415926535boos@gmail.com>
2020-03-11 13:19:11 +0800

    cpufreq: schedhorizon: Hard-code rate-limit thresholds
    Hard-code an uprate-limit of 500 us and a down rate-limit of
    20 ms (which is what userspace currently sets the down
    rate-limit to anyway).
    
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- kernel/sched/cpufreq_schedhorizon.c
+++ kernel/sched/cpufreq_schedhorizon.c
@@ -680,6 +680,9 @@ static ssize_t up_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
 
+	/* Don't let userspace change this */
+	return count;
+
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
 
@@ -699,6 +702,9 @@ static ssize_t down_rate_limit_us_store(struct gov_attr_set *attr_set,
 	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
 	struct sugov_policy *sg_policy;
 	unsigned int rate_limit_us;
+
+	/* Don't let userspace change this */
+	return count;
 
 	if (kstrtouint(buf, 10, &rate_limit_us))
 		return -EINVAL;
@@ -1048,6 +1054,10 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->down_rate_limit_us = 0;
 	tunables->hispeed_load = DEFAULT_HISPEED_LOAD;
 	tunables->hispeed_freq = 0;
+
+	/* Hard-code some sane rate-limit values */
+	tunables->up_rate_limit_us = 500;
+	tunables->down_rate_limit_us = 20000;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
