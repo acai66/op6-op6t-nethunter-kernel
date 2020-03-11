6c35f3463082d574935231fdf9eb5222b5c4d488
boos4721 <3.1415926535boos@gmail.com>
2020-03-11 15:10:34 +0800

    of_batterydata: Add function declarations and fix warning
    Signed-off-by: boos4721 <3.1415926535boos@gmail.com>

--- include/linux/of_batterydata.h
+++ include/linux/of_batterydata.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2014, 2016 The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2014, 2016-2019 The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -46,8 +46,50 @@ int of_batterydata_read_data(struct device_node *container_node,
  * from device tree based on the battery id reading.
  */
 struct device_node *of_batterydata_get_best_profile(
-		struct device_node *batterydata_container_node,
+		const struct device_node *batterydata_container_node,
 		int batt_id_kohm, const char *batt_type);
+
+/**
+ * of_batterydata_get_best_aged_profile() - Find best aged battery profile
+ * @batterydata_container_node: pointer to the battery-data container device
+ *		node containing the profile nodes.
+ * @batt_id_kohm: Battery ID in KOhms for which we want to find the profile.
+ * @batt_age_level: Battery age level.
+ * @avail_age_level: Available battery age level.
+ *
+ * This routine returns a device_node pointer to the closest match battery data
+ * from device tree based on the battery id reading and age level.
+ */
+struct device_node *of_batterydata_get_best_aged_profile(
+		const struct device_node *batterydata_container_node,
+		int batt_id_kohm, int batt_age_level, int *avail_age_level);
+
+/**
+ * of_batterydata_get_aged_profile_count() - Gets the number of aged profiles
+ * @batterydata_node: pointer to the battery-data container device
+ *		node containing the profile nodes.
+ * @batt_id_kohm: Battery ID in KOhms for which we want to find the profile.
+ * @count: Number of aged profiles available to support SOH based profile
+ * loading.
+ *
+ * This routine returns zero if valid number of aged profiles are available.
+ */
+int of_batterydata_get_aged_profile_count(
+		const struct device_node *batterydata_node,
+		int batt_id_kohm, int *count);
+
+/**
+ * of_batterydata_read_soh_aged_profiles() - Reads the data from aged profiles
+ * @batterydata_node: pointer to the battery-data container device
+ *		node containing the profile nodes.
+ * @batt_id_kohm: Battery ID in KOhms for which we want to find the profile.
+ * @soh_data: SOH data from the profile if it is found to be valid.
+ *
+ * This routine returns zero if SOH data of aged profiles is valid.
+ */
+int of_batterydata_read_soh_aged_profiles(
+		const struct device_node *batterydata_node,
+		int batt_id_kohm, struct soh_range *soh_data);
 #else
 static inline int of_batterydata_read_data(struct device_node *container_node,
 				struct bms_battery_data *batt_data,
@@ -56,9 +98,27 @@ static inline int of_batterydata_read_data(struct device_node *container_node,
 	return -ENXIO;
 }
 static inline struct device_node *of_batterydata_get_best_profile(
-		struct device_node *batterydata_container_node,
+		const struct device_node *batterydata_container_node,
 		int batt_id_kohm, const char *batt_type)
 {
-	return -ENXIO;
+	return NULL;
 }
-#endif /* CONFIG_OF_QPNP */
+static inline struct device_node *of_batterydata_get_best_aged_profile(
+		const struct device_node *batterydata_container_node,
+		int batt_id_kohm, u32 batt_age_level)
+{
+	return NULL;
+}
+static inline int of_batterydata_get_aged_profile_count(
+		const struct device_node *batterydata_node,
+		int batt_id_kohm, int *count)
+{
+	return -EINVAL;
+}
+static inline int of_batterydata_read_soh_aged_profiles(
+		const struct device_node *batterydata_node)
+{
+	return -EINVAL;
+}
+#endif /* CONFIG_OF_BATTERYDATA */
+
