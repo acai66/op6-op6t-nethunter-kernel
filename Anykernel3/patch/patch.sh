6d62ebfe8d57e0b0f9191191ee4448a8c43bd349
acai66 <1779864536@qq.com>
2020-05-21 18:46:37 +0800

    fix enable HID functions

--- Anykernel3/patch/patch.sh
+++ Anykernel3/patch/patch.sh
@@ -1,8 +1,18 @@
 #!/system/bin/sh
-# sleep 15
+sleep 3
 mkdir -p /config/usb_gadget/g1/functions/hid.0
 mkdir -p /config/usb_gadget/g1/functions/hid.1
 
 mkdir -p /config/usb_gadget/g1/functions/acm.usb0
 mkdir -p /config/usb_gadget/g1/functions/ecm.usb0
+mkdir -p /config/usb_gadget/g1/functions/hid.0
+echo "1" > /config/usb_gadget/g1/functions/hid.0/protocol
+echo "1" > /config/usb_gadget/g1/functions/hid.0/subclass
+echo "8" > /config/usb_gadget/g1/functions/hid.0/report_length
+cp /keyboard-descriptor.bin /config/usb_gadget/g1/functions/hid.0/report_desc
+mkdir -p /config/usb_gadget/g1/functions/hid.1
+echo "1" > /config/usb_gadget/g1/functions/hid.1/protocol 
+echo "2" > /config/usb_gadget/g1/functions/hid.1/subclass 
+echo "4" > /config/usb_gadget/g1/functions/hid.1/report_length 
+cp /mouse-descriptor.bin /config/usb_gadget/g1/functions/hid.1/report_desc
 
